8961638aacdd107eccd83e6cf8c58863f36d38fb
dheerajndhongade <dheerajndhongade@gmail.com>
2024-01-20 15:21:57 +0530

    registration added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,13 +5,36 @@ import {
   Navigate,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout></Layout>} />
-        <Route path="/search" element={<>Search Page</>} />
+        <Route
+          path="/"
+          element={
+            <Layout>
+              <p>Home page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/search"
+          element={
+            <Layout>
+              <p>Search page</p>
+            </Layout>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <Layout>
+              <Register />
+            </Layout>
+          }
+        />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
     </Router>
--- frontend/src/layouts/Layout.tsx
+++ frontend/src/layouts/Layout.tsx
@@ -1,9 +1,16 @@
+import Footer from "../components/Footer";
 import Header from "../components/Header";
-
-const Layout = () => {
+import Hero from "../components/Hero";
+interface Props {
+  children: React.ReactNode;
+}
+const Layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
+      <Hero />
+      <div className="container mx-auto py-10 flex-1">{children}</div>
+      <Footer />
     </div>
   );
 };
