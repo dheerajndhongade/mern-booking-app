b127565f4883fb52a4bb9e6802d0fe9246b299ce
dheerajndhongade <dheerajndhongade@gmail.com>
2024-01-21 20:47:50 +0530

    validating user added

--- frontend/src/contexts/AppContext.tsx
+++ frontend/src/contexts/AppContext.tsx
@@ -1,5 +1,7 @@
 import React, { useContext, useState } from "react";
 import Toast from "../components/Toast";
+import { useQuery } from "react-query";
+import * as apiClient from "../api-client";
 
 type ToastMeassage = {
   message: string;
@@ -8,6 +10,7 @@ type ToastMeassage = {
 
 type AppContext = {
   showToast: (toastMessage: ToastMeassage) => void;
+  isLoggedIn: boolean;
 };
 
 const AppContext = React.createContext<AppContext | undefined>(undefined);
@@ -18,10 +21,17 @@ export const AppContextProvider = ({
   children: React.ReactNode;
 }) => {
   const [toast, setToast] = useState<ToastMeassage | undefined>(undefined);
+
+  const { isError } = useQuery("validateToken", apiClient.validateToken, {
+    retry: false,
+  });
   return (
     <AppContext.Provider
       value={{
-        showToast: (toastMessage) => setToast(toastMessage),
+        showToast: (toastMessage) => {
+          setToast(toastMessage);
+        },
+        isLoggedIn: !isError,
       }}
     >
       {toast && (
