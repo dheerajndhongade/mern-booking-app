59a3f15c201217e04204b39f242cae16a3da09ce
dheerajndhongade <dheerajndhongade@gmail.com>
2024-01-23 00:01:41 +0530

    sign-in-user validation

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form";
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import * as apiClient from "../api-client";
 import { useAppContext } from "../contexts/AppContext";
 import { useNavigate } from "react-router-dom";
@@ -14,6 +14,7 @@ export type RegisterFormData = {
 
 const Register = () => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
   const { showToast } = useAppContext();
   const {
     register,
@@ -22,8 +23,9 @@ const Register = () => {
     formState: { errors },
   } = useForm<RegisterFormData>();
   const mutation = useMutation(apiClient.register, {
-    onSuccess: () => {
+    onSuccess: async () => {
       showToast({ message: "Registration Success", type: "SUCCESS" });
+      await queryClient.invalidateQueries("validateToken");
       navigate("/");
     },
     onError: (error: Error) => {
